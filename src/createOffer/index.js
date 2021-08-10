a1689ddcb579e21577686e741d53da9ae995c124
Bhaskar Gogoi <b2gogoi@gmail.com>
2021-08-10 18:49:35 +0530

    generate 25 coupons for an offer

--- src/createOffer/index.js
+++ src/createOffer/index.js
@@ -12,6 +12,51 @@ const MERCHANT_SK_PREFIX = 'MERCHANT#';
 const MERCHANT_LOCATION_PK = 'MERCHANT#';
 const MERCHANT_LOCATION_SK_PREFIX = 'MERCHANT_LOCATION#';
 
+const OFFER_COUPON_PK = 'OFFER_COUPON#';
+const COUPON_CODE_SK_PREFIX = 'COUPON#';
+
+/*
+    @param [count] number coupon count, max 9975
+    @param [padChars] string 3 char string for padding
+    @returns Array
+ */
+const padSequence = (count, padChars) => {
+    const paddedSequence = [];
+    for (let i = 0; i < count; i++) {
+        const seq = i + 24;
+        if (seq < 10) {
+            paddedSequence.push(`${padChars}${seq}`);
+        } else if (seq < 100) {
+            paddedSequence.push(`${padChars.substr(0,2)}${seq}`);
+        } else if (seq < 1000) {
+            paddedSequence.push(`${padChars.substr(0,1)}${seq}`);
+        } else {
+            paddedSequence.push(`${seq}`);
+        }
+    }
+    return paddedSequence;
+}
+
+const genCouponCodes = (count, merchant, location, offer) => {
+    const PAD_CHARS = `${offer.offerId.substr(0, 2)}${offer.offerId.substr(offer.offerId.length - 1)}`; // first 2 and last char od 
+    const seq = padSequence(count, PAD_CHARS.toUpperCase());
+
+	const first5 = merchant.category;
+
+    // first 2 letters and last letter
+    const fullMerchantName = merchant.name.split(' ').join('');
+    const second3 = `${fullMerchantName.substr(0,2)}${fullMerchantName.substr(fullMerchantName.length - 1)}`.toUpperCase();
+    const third1 = offer.type.substr(0,1);
+    const fifth3 = `${location.branch.substr(0,2)}${location.branch.substr(location.branch.length - 1)}`.toUpperCase();
+
+    return seq.map((code, i) => {
+        const now = (new Date()).getTime();
+        const addr = now % 10;
+        const last3 = (now + (addr * i) + i) % 1000;
+        const last3Str = `${last3}`;
+        return `${second3}${first5}-${third1}${code}-${fifth3}${last3Str.padStart(3, '0')}`;
+    })
+}
 
 const validateOffer = (offer) => {
     return true;
@@ -25,7 +70,7 @@ exports.handler = async(event) => {
         'Content-Type': 'application/json',
     };
 
-    let merchantId;
+    let merchantId = null;
 
     if (event.queryStringParameters) {
         merchantId = event.queryStringParameters.merchantId;
@@ -48,7 +93,7 @@ exports.handler = async(event) => {
                 };
                 const offersResult = await dynamo.query(queryParams).promise();
                     
-                if ( offersResult && offersResult.Items && offersResult.Items.length > 0) {
+                if (offersResult && offersResult.Items && offersResult.Items.length > 0) {
                     body = offersResult.Items.map(offer => {
                         let clone = { ...offer };
                         delete clone.partitionkey;
@@ -151,7 +196,86 @@ exports.handler = async(event) => {
                 throw new Error(`MerchantId missing in queryParams`);
             }
             break;
-        
+
+        case 'PUT':
+            if (!event.queryStringParameters) {
+                errCode = '400';
+                throw new Error(`OfferId & MerchantId missing in queryParams`);
+            }
+            const { offerId } = event.queryStringParameters;
+            if (offerId && merchantId) {
+                const merchantResult = await dynamo.get({
+                    TableName: TABLE_NAME,
+                    Key: {
+                        partitionkey: MERCHANT_PK,
+                        sortkey: `${MERCHANT_SK_PREFIX}${merchantId}`,
+                    },
+                }).promise();
+                
+                if (!(merchantResult && merchantResult.Item)) {
+                    errCode = '404';
+                    throw new Error(`Not found : merchantId ${merchantId}`);
+                }
+                
+                const merchant = merchantResult.Item;
+                
+                const offerResult = await dynamo.get({
+                    TableName: TABLE_NAME,
+                    Key: {
+                        partitionkey: `${MERCHANT_OFFER_PK}${merchantId}`,
+                        sortkey: `${OFFER_SK_PREFIX}${offerId}`,
+                    },
+                }).promise();
+                
+                if (!(offerResult && offerResult.Item)) {
+                    errCode = '404';
+                    throw new Error(`No such offer exists for OfferId : ${offerId}`);
+                }
+                
+                const offer = offerResult.Item;
+                const { merchantLocationId } = offer;
+
+                const locationResult = await dynamo.get({
+                    TableName: TABLE_NAME,
+                    Key: {
+                        partitionkey: `${MERCHANT_LOCATION_PK}${merchantId}`,
+                        sortkey: `${MERCHANT_LOCATION_SK_PREFIX}${merchantLocationId}`,
+                    },
+                }).promise();
+                
+                const location = locationResult.Item;
+                
+                const { count } = JSON.parse(event.body);
+                
+                const codes = genCouponCodes(count, merchant, location, offer);
+                
+                const requestItems = codes.map(code => ({
+                    PutRequest: {
+                        Item: {
+                            partitionkey: `${OFFER_COUPON_PK}${offerId}`,
+                            sortkey: `${COUPON_CODE_SK_PREFIX}${code}`,
+                            code
+                        },
+                    },
+                }));
+                
+                const result = await dynamo.batchWrite({
+                    RequestItems: {
+                        'shopnob-main-data': requestItems,
+                    },
+                }).promise();
+
+                body = {
+                    result,
+                    success: `${codes.length} coupons generated`,
+                };
+                statusCode = '201';
+            }
+            else {
+                errCode = '400';
+                throw new Error(`OfferId and MerchantId, both are required in queryParams`);
+            }
+            break;
         default:
             throw new Error(`Unsupported method "${event.httpMethod}"`);
         }
